e06879a2d6ba313e0c3e452168f6dfb8804d694e
Biba4444 <egortulinov05@gmail.com>
2024-11-17 12:01:09 +0100

    add: Function with note addition

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,46 @@
-import "./styles.css";
+import "./style.css";
 
 const noteSubmit = document.querySelector<HTMLButtonElement>("#noteSubmit");
 const noteInput = document.querySelector<HTMLInputElement>("#noteInput");
 const noteDescription =
   document.querySelector<HTMLInputElement>("#noteDescription");
 
-noteSubmit!.onclick = event => {
-  event.preventDefault();
-  const block = document.createElement("div");
-  block.classList.add("note-block");
+if (!noteSubmit || !noteInput || !noteDescription) {
+  console.error("Required elements are missing in the DOM!");
+} else {
+  noteSubmit.addEventListener("click", event => {
+    event.preventDefault();
 
-  block.innerHTML = `
-      <div>
-        <p>${noteInput!.value}</p>
-        <div>Additional info</div>
-      </div>
-      <div>
-        <img 
-          width="15px" 
-          height="15px" 
-          src="https://img.icons8.com/ios-glyphs/30/multiply.png" 
-          alt="multiply"
-        />
-      </div>
-    `;
+    if (noteInput.value.length > 0 && noteDescription.value.length > 0) {
+      const block = document.createElement("div");
+      block.classList.add("note-block");
 
-  const rightContainer = document.querySelector<HTMLElement>("#rightContainer");
-  if (rightContainer) {
-    rightContainer.appendChild(block);
-  } else {
-    console.error("rightContainer not found in the DOM!");
-  }
-  noteInput!.value = "";
-};
+      block.innerHTML = `
+            <div>
+              <p>${noteInput.value}</p>
+              <div>${noteDescription.value}</div>
+            </div>
+            <div>
+              <img 
+                width="15px" 
+                height="15px" 
+                src="https://img.icons8.com/ios-glyphs/30/multiply.png" 
+                alt="multiply"
+              />
+            </div>
+          `;
+
+      const rightContainer =
+        document.querySelector<HTMLElement>("#rightContainer");
+      if (rightContainer) {
+        rightContainer.appendChild(block);
+      } else {
+        console.error("rightContainer not found in the DOM!");
+      }
+      noteInput.value = "";
+      noteDescription.value = "";
+    } else {
+      console.log("Write your note firstly");
+    }
+  });
+}
