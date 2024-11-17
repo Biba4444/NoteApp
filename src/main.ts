c66f1e8dee186a96ce5b7478176bc4420a911d55
Biba4444 <egortulinov05@gmail.com>
2024-11-17 11:34:13 +0100

    add: exoskeleton for my code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
+import "./styles.css";
 
+const noteSubmit = document.querySelector<HTMLButtonElement>("#noteSubmit");
+const noteInput = document.querySelector<HTMLInputElement>("#noteInput");
+const noteDescription =
+  document.querySelector<HTMLInputElement>("#noteDescription");
+
+noteSubmit!.onclick = event => {
+  event.preventDefault();
+  const block = document.createElement("div");
+  block.classList.add("note-block");
+
+  block.innerHTML = `
+      <div>
+        <p>${noteInput!.value}</p>
+        <div>Additional info</div>
+      </div>
+      <div>
+        <img 
+          width="15px" 
+          height="15px" 
+          src="https://img.icons8.com/ios-glyphs/30/multiply.png" 
+          alt="multiply"
+        />
+      </div>
+    `;
+
+  const rightContainer = document.querySelector<HTMLElement>("#rightContainer");
+  if (rightContainer) {
+    rightContainer.appendChild(block);
+  } else {
+    console.error("rightContainer not found in the DOM!");
+  }
+  noteInput!.value = "";
+};
